5aff403a21da7e7d78f746a4b66462b70f6e6c18
Aviv jan <avivjan2000@gmail.com>
2020-10-21 21:31:45 +0300

    Create UserLogin resource

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from security import authenticate, identity
 from db import db
 
+from resoures.User_login import UserLogin
 from resoures.user_register import UserRegister
 from resoures.item_list import ItemsList
 from resoures.item import Item
@@ -20,13 +20,7 @@ app.config["PROPAGATE_EXCEPTIONS"] = True
 app.secret_key = "Hecktor"
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
-jwt = JWT(app, authenticate, identity)  # /auth Endpoint
+jwt = JWTManager(app)
 
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemsList, "/items")
@@ -34,8 +28,14 @@ api.add_resource(UserRegister, "/register")
 api.add_resource(Store, "/store/<string:name>")
 api.add_resource(StoreList, "/stores")
 api.add_resource(User, "/user/<int:user_id>")
-
+api.add_resource(UserLogin, "/login")
 db.init_app(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
