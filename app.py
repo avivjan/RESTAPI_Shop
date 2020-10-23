dca8ed188c955b9ad062cee64786d1bbad9fef19
Aviv jan <avivjan2000@gmail.com>
2020-10-23 18:52:32 +0300

    Add JWT config to app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")   # If the envarioment var is not assigned use the local SQlite
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(seconds=30)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(minutes=5)
 app.secret_key = "Hecktor"
 api = Api(app)
 
@@ -37,11 +37,13 @@ api.add_resource(TokenRefresh, "/refresh")
 db.init_app(app)
 
 
+# This wiil be called before executing the first request.
 @app.before_first_request
 def create_tables():
     db.create_all()
 
 
+# This wiil be called every time a token is created, the data that returns here will add to the token as a claim.
 @jwt.user_claims_loader
 def add_claims_to_jwt(identity):
     if identity == 1:
@@ -49,5 +51,50 @@ def add_claims_to_jwt(identity):
     return {"is_admin": False}
 
 
+# This will be called if client send an expired token.
+@jwt.expired_token_loader
+def expired_token_callback():
+    return {
+        "description": "This token has expired",
+        "error": "token_expired"
+    },401
+
+# This will be called if client send an invalid token.
+@jwt.invalid_token_loader
+def invalid_token_callback(error)
+    return {
+        "description": "Signiture verification failed",
+        "error": "invalid_token"
+    }, 401
+
+
+# This will be called if client dont send a token at all.
+@jwt.unauthorized_loader
+def missing_token_callback():
+    return {
+               "description": "Request does not contain an access token",
+               "error": "authorization_required"
+           }, 401
+
+# This will be called if this method required a fresh token and client send a non-fresh token.
+@jwt.needs_fresh_token_loader
+def token_not_fresh_token():
+    return {
+               "description": "The token is not fresh",
+               "error": "fresh_token_required"
+           }, 401
+
+# This will be called if client send a revoked token.
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return {
+               "description": "This token has been revoked",
+               "error": "token_revoked"
+           }, 401
+
+
+
+
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
+
