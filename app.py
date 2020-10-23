f1226d541549f064ade6f2ad09f977a29ade5404
Aviv jan <avivjan2000@gmail.com>
2020-10-23 18:19:55 +0300

    Required fresh  jwt
    Required fresh jwt in post method in item resource

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
+import datetime
 
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from db import db
 
-from resoures.User_login import UserLogin
+from resoures.user_login import UserLogin
 from resoures.user_register import UserRegister
+from resoures.token_refresh import TokenRefresh
 from resoures.item_list import ItemsList
 from resoures.item import Item
 from resoures.store import Store
@@ -17,6 +19,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")   # If the envarioment var is not assigned use the local SQlite
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(seconds=30)
 app.secret_key = "Hecktor"
 api = Api(app)
 
@@ -29,6 +32,8 @@ api.add_resource(Store, "/store/<string:name>")
 api.add_resource(StoreList, "/stores")
 api.add_resource(User, "/user/<int:user_id>")
 api.add_resource(UserLogin, "/login")
+api.add_resource(TokenRefresh, "/refresh")
+
 db.init_app(app)
 
 
--- resoures/item.py
+++ resoures/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, fresh_jwt_required
 
 from models.itemmodel import ItemModel
 
@@ -18,6 +18,7 @@ class Item(Resource):
             return requested_item.json()
         return {"message": "Item not found"}, 404
 
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
